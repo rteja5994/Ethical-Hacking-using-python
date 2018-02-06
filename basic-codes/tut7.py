eb6c50e69ee5854f308e36a60b950c00cbd00b8a
rteja5994 <raviteja.chunduri@dal.ca>
2018-02-06 16:28:44 -0400

    to know about write and how to write in a file using python

--- basic-codes/tut7.py
+++ basic-codes/tut7.py
@@ -1,10 +1,11 @@
-#to know about files "r-read" , 
+#to know about files "r-read" , "w-write", "r+ - read and write" 
 #to know about stripping i.e., line.strip('\n \r')
 
 def Main():
-	f = open("myfile.txt", 'r')
-	for line in f:
+	f = open("myfile.txt", 'w') #to use write mode
+	for i in range(4):
 	#we are using "for" loop because python knows how many lines are in the file
+		f.write(input("please enter a word: ") + '\n')
 		print(line.strip()) #we used stripping here to clear the unwanted spaces
 	f.close() #to close our file
 
