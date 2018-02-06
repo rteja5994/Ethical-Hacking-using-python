349720baf7f6862e66491ca82f4523f61c9a903c
rteja5994 <raviteja.chunduri@dal.ca>
2018-02-06 16:32:59 -0400

    to use with and open a file

--- basic-codes/tut7.py
+++ basic-codes/tut7.py
@@ -1,13 +1,13 @@
 #to know about files "r-read" , "w-write", "r+ - read and write" 
 #to know about stripping i.e., line.strip('\n \r')
+#using with , with automatically closes the book
 
 def Main():
-	f = open("myfile.txt", 'w') #to use write mode
-	for i in range(4):
-	#we are using "for" loop because python knows how many lines are in the file
-		f.write(input("please enter a word: ") + '\n')
-		print(line.strip()) #we used stripping here to clear the unwanted spaces
-	f.close() #to close our file
+	words = ['cat','bat','mat','rat']
+	with open("myfile.txt", 'w') as f:
+		for word in words:
+			f.write(word + '\n')
+	
 
 if __name__=="__main__":
 	Main()
