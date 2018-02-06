47774106d0574934c3f11554f823a7dfb7050c79
rteja5994 <raviteja.chunduri@dal.ca>
2018-02-06 16:02:38 -0400

    to know about pass by reference by initialising prompt

--- basic-codes/tut5.py
+++ basic-codes/tut5.py
@@ -1,12 +1,13 @@
 #to use functions and why main functions is used in programming
 
-def getinteger():
-	result = int(input("Please enter an integer: "))
+def getinteger(prompt):
+	#we used prompt to use pass by reference
+	result = int(input(prompt))
 	return result
 
 def Main():
 	print("Started")
-	output = getinteger()
+	output = getinteger("Please enter an integer:")
 	print(output)
 
 if __name__=="__main__":
