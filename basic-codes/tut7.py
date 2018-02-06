aebeeada330c0008447a0d6909bb1ae53ab4f631
rteja5994 <raviteja.chunduri@dal.ca>
2018-02-06 16:24:05 -0400

    to know about stripping

--- basic-codes/tut7.py
+++ basic-codes/tut7.py
@@ -1,10 +1,11 @@
-#to know about files
+#to know about files "r-read" , 
+#to know about stripping i.e., line.strip('\n \r')
 
 def Main():
 	f = open("myfile.txt", 'r')
 	for line in f:
 	#we are using "for" loop because python knows how many lines are in the file
-		print(line)
+		print(line.strip()) #we used stripping here to clear the unwanted spaces
 	f.close() #to close our file
 
 if __name__=="__main__":
