32c02abeb19a23b635f0a5414366b639bfd8af1f
rteja5994 <raviteja.chunduri@dal.ca>
2018-02-06 16:07:10 -0400

    to know what is iteration and what are used in iterations

---
+++ basic-codes/tut6.py
@@ -0,0 +1,10 @@
+#iteration
+#use of for and while loops
+#use of turtles and drawing a square is perfect example to learn iteration
+
+def Main():
+	for step in range(5):
+		print(step)
+
+if __name__=="__main__":
+	Main()
