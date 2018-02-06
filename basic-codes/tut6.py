e972c8f202db717da12df4b827d6b7ced216ae6a
rteja5994 <raviteja.chunduri@dal.ca>
2018-02-06 16:09:31 -0400

    about for loops in interation and how to print lists using for loop

--- basic-codes/tut6.py
+++ basic-codes/tut6.py
@@ -6,5 +6,10 @@ def Main():
 	for step in range(5):
 		print(step)
 
+	words = ['cat','bat','hat','rat','sat']
+	#the above words is an example of list
+
+	for word in words:
+		print(word)
 if __name__=="__main__":
 	Main()
